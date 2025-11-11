6d452ad8533855bdf7f3bf5221d0248ce4e64eec
yaninaf89 <yaninafrias1989@gmail.com>
2025-11-11 19:51:10 -0300

    modifique test

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Archivo principal del grupo
-# Cada integrante deberá agregar su función en un archivo independiente
+# Cada integrante debera agregar su funcion en un archivo independiente
 from funciones import *
 
 if __name__ == "__main__":
-    print("Ejecución de pruebas de las funciones del grupo")
+    print("Ejecucion de pruebas de las funciones del grupo")
