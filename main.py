ad6cea0eee9b01437917f9f22b2350c3ef36c465
yaninaf89 <yaninafrias1989@gmail.com>
2025-11-11 20:04:01 -0300

    Delete main.py

--- main.py
+++
@@ -1,6 +0,0 @@
-# Archivo principal del grupo
-# Cada integrante debera agregar su funcion en un archivo independiente
-from funciones import *
-
-if __name__ == "__main__":
-    print("Ejecucion de pruebas de las funciones del grupo")
